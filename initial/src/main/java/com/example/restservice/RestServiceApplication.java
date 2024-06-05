4b6045b838debf8d022cdff4210c323d1ee4bf04
FarmerCodeY <132327gg@gmail.com>
2024-06-05 17:00:43 +0800

    "complete restful service"

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,6 +2,11 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class RestServiceApplication {
